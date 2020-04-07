c505e763e5f075807bc03228fc01a1e37e785eea
Michael Große <mic.grosse@googlemail.com>
2020-04-07 15:55:43 +0200

    Add script to scrape date from rki webpage

---
+++ scripts/scrapeData.js
@@ -0,0 +1,50 @@
+const https = require('https');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const fs = require('fs');
+
+const rkiURL = 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html';
+
+https.get(rkiURL, (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    parseHTML(data);
+  });
+});
+
+function parseHTML(html){
+  const dom = new JSDOM(html);
+  const data = [ [ 'State','Confirmed','Deaths' ] ];
+
+  dom.window.document.querySelectorAll('tbody tr').forEach(
+    (tr) => {
+      const statename = tr.children[0].textContent
+        .replace(/[^\wüöä-]/ug, '')
+      ;
+      const infected = tr.children[1].textContent.replace('.', '');
+      const deaths = tr.children[4].textContent.replace('.', '');
+      data.push([`"${statename}"`, infected, deaths]);
+    }
+  )
+  // Remove the final "Gesamt" line
+  data.pop();
+
+  const dateString = extractDateAsString(html);
+
+  const text = data.map( stateData => stateData.join(',')).join("\n");
+
+  fs.writeFile(`daily_reports/${dateString}.csv`, text, 'utf8', () => console.log('done'))
+}
+
+function extractDateAsString(html) {
+  const dateMatch = html.match(/Stand: (?<day>\d{1,2})\.(?<month>\d{1,2})\.(?<year>\d{4}), \d\d:\d\d Uhr/);
+  const { day, month, year } = dateMatch.groups;
+  return `${year}-${month.padStart(2,0)}-${day.padStart(2,0)}`;
+}
